1ce148fbe1f8fd4f46b4385f67057e5b314f442c
stas <ibop0115011@sibupk.su>
2022-03-25 00:33:54 +0700

    NewCommit

---
+++ VS1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        void func1(double n)
+        {
+            Console.WriteLine("{0}", n);
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.func1(95.4);
+        }
+    }
+}
